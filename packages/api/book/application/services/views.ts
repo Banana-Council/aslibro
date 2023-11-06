96e8c7e7eb5af2e8b871f0b84697f471a4ac673d
Davison27 <davidencinarejo27@gmail.com>
2023-11-07 00:30:56 +0100

    feat(api): update services

--- packages/api/book/application/services/views.ts
+++ packages/api/book/application/services/views.ts
@@ -2,6 +2,7 @@ import BookView from '../models/view'
 
 type BookViews = Readonly<{
   add: (view: BookView) => Promise<void>
+  delete: (id: string) => Promise<void>
 }>
 
 const BookViews = 'BookViews'
--- packages/api/book/domain/services/books.ts
+++ packages/api/book/domain/services/books.ts
@@ -1,7 +1,12 @@
+import Either from '~/shared/either'
+
+import NotFoundBook from '../exceptions/not-found'
 import Book from '../models/book'
+import BookId from '../models/id'
 
 type Books = Readonly<{
   save(book: Book): Book
+  withId(bookId: BookId): Promise<Either<NotFoundBook, Book>>
 }>
 
 export default Books
